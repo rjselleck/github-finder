60c5a9622e05d7d31b076af610c70dfaf44a9a80
Robert Selleck <selleck1@lsu.edu>
2020-09-11 15:42:16 -0500

    refactored - uses context api, useContext hook, useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -6,95 +6,35 @@ import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-import axios from 'axios'
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 import "./App.css";
 
 const App = () => {
 
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-
-
-  // Search Github Users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
-  //Get single GitHub user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}`);
-
-    setUser(res.data);
-    setLoading(false);
-  };
-
-  // Get user's repos
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`);
-
-    setRepos(res.data);
-    setLoading(false);
-  };
-  // Clear users from state
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
-  };
-  // Set alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg: msg, type: type });
-    setTimeout(() => setAlert(null), 5000);
-  };
-
   return (
-    <Router>
-      <div className='App'>
-        <Navbar />
-        <div className='container'>
-          <Alert alert={alert} />
-          <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search
-                  searchUsers={searchUsers}
-                  clearUsers={clearUsers}
-                  showClear={users.length > 0 ? true : false}
-                  setAlert={showAlert}
-                />
-                <Users
-                  loading={loading}
-                  users={users}
-                />
-              </Fragment>
-            )} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/user/:login' render={props => (
-              <User
-                {...props}
-                getUser={getUser}
-                getUserRepos={getUserRepos}
-                user={user}
-                repos={repos}
-                loading={loading}
-              />
-            )} />
-          </Switch>
-
-        </div>
-      </div>
-    </Router>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' render={props => (
+                  <Fragment>
+                    <Search />
+                    <Users />
+                  </Fragment>
+                )} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
 }
 
